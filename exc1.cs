6d414ff6fbbd3d78aa75308e332afacd8d71f910
Ildar Mulyukov <ildar@altlinux.ru>
2005-10-25 22:47:52 +0600

    commiting old works

---
+++ exc1.cs
@@ -0,0 +1,38 @@
+using System;
+using Arsac;
+
+public class Exercise1 {
+    public static int Main() {
+	const int seq_length = 5000;
+	const int int_num = 50;
+	
+	double b;
+	string input;
+	
+	Console.Write("Enter first number: ");
+	input = Console.ReadLine();
+	b = Double.Parse(input);
+	if(b<0 || b>=1) {
+	    Console.WriteLine("Bad input, exiting: {0}", b);
+	    return 1;
+	}
+
+	int[] intervals = new int[int_num];
+	for(int i=0; i<seq_length; i++) {
+	    intervals[ (int) (b*int_num) ]++;
+	    b = ArsacFunctions.random(b);
+	}
+
+	int max=intervals[0], min=intervals[0];
+	for(int i=0; i<int_num; i++) {
+	    Console.Write("{0} ", intervals[i]);
+	    if(intervals[i]>max) max=intervals[i];
+	    if(intervals[i]<min) min=intervals[i];
+	}
+
+        Console.WriteLine("");
+        Console.WriteLine("Max = {0}, Min = {1}", max, min);
+			
+	return 0;
+    }
+}
