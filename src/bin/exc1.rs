3f79d632e1fde68703a97094c49d154e177aab6e
Ildar Mulyukov <ildar@altlinux.ru>
2021-05-09 02:30:01 +0600

    exc1: add using initial seed from the command line (optional)

--- src/bin/exc1.rs
+++ src/bin/exc1.rs
@@ -2,17 +2,27 @@
 
 const SAMPLES_NUM: u32 = 5000;
 const INTERVALS_NUM: usize = 50;
-const B_INIT: f32 = 0.123;
+const B_INIT_DEFAULT: f32 = 0.123;
 
 use arsak_zhak_programmirovanie_igr_i_golovolomok::*;
+use std::env;
+use std::str::FromStr;
 
 fn main() {
-    let mut b = B_INIT;
+    let args: Vec<String> = env::args().collect();
+    let mut b = if args.len() == 1 {
+        B_INIT_DEFAULT
+    } else {
+        match f32::from_str(&args[1]) {
+            Ok(res) => res,
+            _ => B_INIT_DEFAULT,
+        }
+    };
     let mut intervals = [0; INTERVALS_NUM];
 
     for _ in 0..SAMPLES_NUM {
         b = random(b);
-        intervals[(b*(INTERVALS_NUM as f32)).trunc() as usize] += 1;
+        intervals[(b * (INTERVALS_NUM as f32)).trunc() as usize] += 1;
     }
     println!("is it even?");
     for i in 0..INTERVALS_NUM {
