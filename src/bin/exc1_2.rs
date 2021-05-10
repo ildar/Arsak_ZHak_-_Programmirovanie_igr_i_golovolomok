e5609fa8d30683b758ad403c3a4d57cf462fb28a
Ildar Mulyukov <ildar@altlinux.ru>
2021-05-10 15:14:57 +0600

    exc1_2: in one program for both exercies 1 and 2

--- src/bin/exc1_2.rs
+++ src/bin/exc1_2.rs
@@ -1,4 +1,4 @@
-// Exercise 1 (p.13 of the book)
+// Exercises 1&2 (p.13 of the book)
 
 const SAMPLES_NUM: u32 = 5000;
 const INTERVALS_NUM: usize = 50;
@@ -8,6 +8,7 @@ use arsak_zhak_programmirovanie_igr_i_golovolomok::*;
 use std::env;
 use std::str::FromStr;
 
+// Run as exc1 [initial_seed [randomization_param]]
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut b = if args.len() == 1 {
@@ -18,10 +19,26 @@ fn main() {
             _ => B_INIT_DEFAULT,
         }
     };
+    let a_param = if args.len() > 2 {
+        match f32::from_str(&args[2]) {
+            Ok(res) => res,
+            _ => f32::NAN,
+        }
+    } else {
+        f32::NAN
+    };
+    let fn_random = |seed| {
+        if a_param != a_param { // NaN
+            random(seed)
+        } else {
+            random_with_param(seed, a_param)
+        }
+    };
+
     let mut intervals = [0; INTERVALS_NUM];
 
     for _ in 0..SAMPLES_NUM {
-        b = random(b);
+        b = fn_random(b);
         intervals[(b * (INTERVALS_NUM as f32)).trunc() as usize] += 1;
     }
     println!("is it even?");
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 // Common part
 
-pub fn random(seed: f32) -> f32 {
-    let newval = (seed+3.14159).powf(8.);
+pub fn random_with_param(seed: f32, a: f32) -> f32 {
+    let newval = (seed + a).powf(8.);
     newval - newval.trunc()
 }
 
+pub fn random(seed: f32) -> f32 {
+    random_with_param(seed, 3.14159)
+}
